dbd44116ee76532b642f319652b28720b29586db
Iñigo Blanco <inigo.blanco@logalty.com>
2026-06-17 06:14:48 +0200

    Corrected scripts

---
+++ src/archivematica/storage_service/locations/migrations/0039_Logalty.py
@@ -0,0 +1,55 @@
+from django.db import migrations
+from django.db import models
+
+
+class Migration(migrations.Migration):
+    dependencies = [("locations", "0038_Logalty")]
+    operations = [
+        migrations.AddField(
+            model_name="logalty",
+            name="s3_endpoint_url",
+            field=models.CharField(
+                help_text=b"The URL for AWS S3 on which upload the files before encryption",
+                max_length=2048,
+                default="",
+                preserve_default=False,
+            ),
+        ),
+        migrations.AddField(
+            model_name="logalty",
+            name="s3_region",
+            field=models.CharField(
+                help_text=b"The REGION for AWS S3 on which upload the files before encryption",
+                max_length=64,
+                default="",
+                preserve_default=False,
+            ),
+        ),
+        migrations.AddField(
+            model_name="logalty",
+            name="s3_access_key_id",
+            field=models.CharField(
+                help_text=b"The USER_ID for AWS S3 on which upload the files before encryption",
+                max_length=64,
+                blank=True,
+            ),
+        ),
+        migrations.AddField(
+            model_name="logalty",
+            name="s3_secret_access_key",
+            field=models.CharField(
+                help_text=b"The SECRET for AWS S3 on which upload the files before encryption",
+                max_length=256,
+                blank=True,
+            ),
+        ),
+        migrations.AddField(
+            model_name="logalty",
+            name="s3_bucket",
+            field=models.CharField(
+                help_text=b"The s3 BUCKET for AWS S3 on which upload the files before encryption",
+                max_length=64,
+                blank=True,
+            ),
+        ),
+    ]
